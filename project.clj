2992f9c0026ece35853ec3a69dd296486ecf4534
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-11-02 11:47:10 +0100

    Updated project setup: description, bump up Clojure version, added devel plugins

--- project.clj
+++ project.clj
@@ -1,6 +1,21 @@
+;
+;  (C) Copyright 2018  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (defproject clj-rest-api-utils "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+    :description "Utility functions for handling REST API calls."
+    :url "http://example.com/FIXME"
+    :license {:name "Eclipse Public License"
+              :url "http://www.eclipse.org/legal/epl-v10.html"}
+    :dependencies [[org.clojure/clojure "1.8.0"]]
+    :plugins [[test2junit "1.1.0"]
+              [codox "0.8.11"]
+              [lein-cloverage "1.0.7-SNAPSHOT"]])
