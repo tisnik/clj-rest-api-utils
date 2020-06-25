f785eb06a99bb825afe350faa5a25aa98a94019d
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-25 08:14:12 +0200

    Updated (c) year

--- test/clj_rest_api_utils/rest_api_utils_test.clj
+++ test/clj_rest_api_utils/rest_api_utils_test.clj
@@ -1,3 +1,15 @@
+;
+;  (C) Copyright 2017, 2018, 2019, 2020  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (ns clj-rest-api-utils.rest-api-utils-test
   (:require [clojure.test :refer :all]
             [clj-rest-api-utils.rest-api-utils :refer :all]))
