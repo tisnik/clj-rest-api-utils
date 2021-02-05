95f5cbe810323def9a5f0da43cd2df47d8038410
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-05 08:23:20 +0100

    New dependency - lein-codox

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;
-;  (C) Copyright 2018, 2020  Pavel Tisnovsky
+;  (C) Copyright 2018, 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -16,6 +16,7 @@
   :license {:name "Eclipse Public License",
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :plugins [[test2junit "1.1.0"]
+  :plugins [[lein-codox "0.10.7"]
+            [test2junit "1.1.0"]
             [codox "0.8.11"]
             [lein-cloverage "1.0.7-SNAPSHOT"]])
